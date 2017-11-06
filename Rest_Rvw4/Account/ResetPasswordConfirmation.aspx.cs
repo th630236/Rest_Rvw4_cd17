0a4e5230d35db1d9e8352b06e33e1b1e5849c5a4
Timothy M Hitchcock <timh412@inebraska.com>
2017-11-06 15:22:02 -0600

    Add project files.

---
+++ Rest_Rvw4/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Rest_Rvw4.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Rest_Rvw4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rest_Rvw4.Startup))]
+namespace Rest_Rvw4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
